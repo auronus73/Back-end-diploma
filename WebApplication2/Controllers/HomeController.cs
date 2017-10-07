033d46b980f96cfe4910ef6b933df5244325b4ef
auronus73 <auronus@mail.ru>
2017-10-07 08:14:16 +0400

    Изменение мастер-страницы

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -11,30 +11,17 @@ namespace WebApplication2.Controllers
 {
     public class HomeController : Controller
     {
-        EventContext db_event = new EventContext();
-        StudentContext db_student = new StudentContext();
-
         public ActionResult Index()
         {
             var asd = new diplomaEntities();
             var qwe = asd.Students;
-            
-            // получаем из бд все объекты Book
-            IEnumerable<Student> students = db_student.Students;
-            // передаем все объекты в динамическое свойство Books в ViewBag
-            ViewBag.Students = students;
-            // возвращаем представление
             return View(qwe);
         }
 
         [HttpGet]
-        public ActionResult EditStudent(int? id)
+        public ActionResult EditStudent(int id)
         {
             var asd = new diplomaEntities();
-            if (id == null)
-            {
-                return HttpNotFound();
-            }
             Student student = asd.Students.Find(id);
             if (student != null)
             {
@@ -48,7 +35,6 @@ namespace WebApplication2.Controllers
         {
             var asd = new diplomaEntities();
             asd.Entry(student).State = EntityState.Modified;
-            
             // сохраняем в бд все изменения
             asd.SaveChanges();
             return RedirectToAction("Index");
@@ -57,7 +43,6 @@ namespace WebApplication2.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
